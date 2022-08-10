b62e4c88339ec482fbc6b5fa463ea6c54ec0b0fe
Bruno Gonçalves de Alencar <brunoalencar2002@hotmail.com>
2022-08-10 14:24:21 -0300

    first commit

---
+++ src/main/java/com/digitalinnovation/springdatacpf/SpringDataCpfApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.springdatacpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataCpfApplication.class, args);
+	}
+
+}
